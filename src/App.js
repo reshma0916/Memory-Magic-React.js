5e3ba5d426c24537307fb68470b7c805d722f614
reshma0916 <pg246104@gmail.com>
2024-10-22 15:20:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
+import { useEffect, useState, useStatue } from 'react'
 import './App.css';
+import SingleCard from './Components/SingleCard';
+
+const cardImages = [
+  { "src": "/img/clock-1.jpg", matched: false },
+  { "src": "/img/airplane-1.jpg",  matched: false},
+  { "src": "/img/cup-1.jpg",  matched: false},
+  { "src": "/img/goldbar-1.jpg" , matched: false},
+  { "src": "/img/flowers-1.jpg",  matched: false},
+  { "src": "/img/umbrellas-1.jpg" , matched: false},
+  { "src": "/img/pen-1.jpg",  matched: false },
+  { "src": "/img/penguin-1.jpg" , matched: false }
+]
 
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
+
+  const shuffleCards = () => {
+    const shuffleCards = [...cardImages, ...cardImages]
+    .sort(() => Math.random() - 0.5)
+    .map((card) => ({...card, id: Math.random() }))
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setCards(shuffleCards)
+    setTurns(0)
+
+  }
+
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+// 2 select chesina cards ni compare dng ekkada
+  useEffect(() => {  
+    if(choiceOne && choiceTwo) {
+      setDisabled(true)
+
+      if(choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if(card.src === choiceOne.src) {
+              return {...card, matched: true}
+            } else {
+              return card
+            }
+          })
+        })
+        
+        resetTurn()
+      } else {
+       
+        setTimeout(() => resetTurn(), 1000) 
+      }
+    }
+
+  }, [choiceOne, choiceTwo])
+
+  console.log(cards)
+//ekkada reset gurinchi inka turns kosam evi use chesam
+const resetTurn = () => {
+  setChoiceOne(null)
+  setChoiceTwo(null)
+  setTurns(prevTurns => prevTurns + 1)
+  setDisabled(false)
+}
+
+//new game ni automatic ga strt chydaniki
+useEffect(() => {
+   shuffleCards()
+}, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card => (
+          <SingleCard 
+          key={card.id} 
+          card={card} 
+          handleChoice={handleChoice} 
+          flipped={card === choiceOne || card === choiceTwo || card.matched}
+          disabled={disabled}
+          />
+        ))}
+      </div>
+      <p>Turns: {turns}</p>
     </div>
   );
 }
